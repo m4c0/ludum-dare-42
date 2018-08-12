b26703e05726f642f758703111ededf6524f0c53
Eduardo Costa <m4c0@users.noreply.github.com>
2018-08-12 15:57:30 +0100

    cubes are now spawning, and the game looks funnier than expected

---
+++ Azaron/Assets/Scripts/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public float spawnTimeout = 10.0f;
+
+    public Renderer platform;
+
+    public Material ok;
+    public Material nok;
+    public Material warn;
+
+    public Cube cubePrefab;
+
+    private int enterCount = 0;
+
+    private void OnEnable() {
+        StartCoroutine(SpawnLoop());
+    }
+
+    private void Update() {
+        if (enterCount == 0) {
+            platform.sharedMaterial = ok;
+        } else {
+            platform.sharedMaterial = warn;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        enterCount++;
+    }
+    private void OnTriggerExit(Collider other) {
+        enterCount--;
+    }
+
+    private IEnumerator SpawnLoop() {
+        while (isActiveAndEnabled) {
+            Spawn();
+            yield return new WaitForSeconds(spawnTimeout);
+        }
+    }
+
+    private void Spawn() {
+        var cube = Instantiate(cubePrefab);
+        cube.transform.position = transform.position;
+    }
+}
