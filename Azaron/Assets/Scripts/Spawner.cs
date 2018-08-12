41f24153309e18e3a9872b1b3bd3ef930f055485
Eduardo Costa <m4c0@users.noreply.github.com>
2018-08-12 16:24:22 +0100

    spawn with alarm

--- Azaron/Assets/Scripts/Spawner.cs
+++ Azaron/Assets/Scripts/Spawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
     public float spawnTimeout = 10.0f;
+    public float alarmTimeout = 10.0f;
 
     public Renderer platform;
 
@@ -14,16 +15,19 @@ public class Spawner : MonoBehaviour {
     public Cube cubePrefab;
 
     private int enterCount = 0;
+    private bool readyToSpawn = false;
 
     private void OnEnable() {
         StartCoroutine(SpawnLoop());
     }
 
     private void Update() {
-        if (enterCount == 0) {
-            platform.sharedMaterial = ok;
-        } else {
+        if (enterCount != 0) {
+            platform.sharedMaterial = nok;
+        } else if (readyToSpawn) {
             platform.sharedMaterial = warn;
+        } else {
+            platform.sharedMaterial = ok;
         }
     }
 
@@ -36,8 +40,11 @@ public class Spawner : MonoBehaviour {
 
     private IEnumerator SpawnLoop() {
         while (isActiveAndEnabled) {
-            Spawn();
+            readyToSpawn = false;
+            yield return new WaitForSeconds(spawnTimeout - alarmTimeout);
+            readyToSpawn = true;
             yield return new WaitForSeconds(spawnTimeout);
+            Spawn();
         }
     }
 
